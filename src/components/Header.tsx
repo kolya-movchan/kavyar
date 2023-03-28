ebfee2ad8ac124c3cfc7ffa533f23ce7fe9d5188
Kolya Movchan <kmovchan261@gmail.com>
2023-03-28 15:15:17 +0300

    Implement SCSS reset, add header langs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,18 +3,29 @@ import '../styles/main.scss';
 
 export const Header: React.FC = () => {
   return (
-    <div className="header">
-      <div className="header__logo-container">
-        <a className="header__title" href="/">
-          Кав’яр
-        </a>
+    <>
+      <div className="header">
+        <div className="header__container">
+          <a className="header__logo-container logo" href="/">
+            <span className="logo__title">
+              Кав’яр
+            </span>
 
-        <div className="header__logo-container logo">
-          <a href="/" className="logo__link">
             <img className="logo__image" src="/coffe-grain-logo.svg" alt="logo" />
           </a>
+
+          <div className="header__language-section">
+            <a href="/" className="header__language header__language-eng">
+              eng
+            </a>
+
+            <span className="header__language">|</span>
+            <a href="/" className="header__language header__language-ua">
+              ua
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
