39164afbb2e0db50407480b39c656a3624712017
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-04-05 20:04:01 +0300

    Change filter option to use id

--- src/main/java/ua/kavyar/repository/specification/coffeeShop/CoffeeShopFilterSpecificationProvider.java
+++ src/main/java/ua/kavyar/repository/specification/coffeeShop/CoffeeShopFilterSpecificationProvider.java
@@ -1,13 +1,13 @@
 package ua.kavyar.repository.specification.coffeeShop;
 
-import ua.kavyar.model.CoffeeShop;
-import ua.kavyar.model.Feature;
-import ua.kavyar.repository.specification.SpecificationProvider;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.JoinType;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
+import ua.kavyar.model.CoffeeShop;
+import ua.kavyar.model.Feature;
+import ua.kavyar.repository.specification.SpecificationProvider;
 
 @Component
 public class CoffeeShopFilterSpecificationProvider implements SpecificationProvider<CoffeeShop> {
@@ -15,12 +15,12 @@ public class CoffeeShopFilterSpecificationProvider implements SpecificationProvi
     private static final String FIELD_NAME = "features";
 
     @Override
-    public Specification<CoffeeShop> getSpecification(String[] features) {
+    public Specification<CoffeeShop> getSpecification(String[] featureIds) {
         return (root, query, cb) -> {
             Join<CoffeeShop, Feature> featuresJoin = root.join(FIELD_NAME, JoinType.LEFT);
-            CriteriaBuilder.In<String> predicate = cb.in(featuresJoin.get("name"));
-            for (String feature : features) {
-                predicate.value(feature);
+            CriteriaBuilder.In<Long> predicate = cb.in(featuresJoin.get("id"));
+            for (String featureId : featureIds) {
+                predicate.value(Long.valueOf(featureId));
             }
             return cb.and(predicate);
         };
