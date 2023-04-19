b757fe4ee7b7bb2693c3d0ac925a76be96e05993
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-04-19 18:06:34 +0300

    Add correct login check

--- src/main/java/ua/kavyar/service/impl/UserServiceImpl.java
+++ src/main/java/ua/kavyar/service/impl/UserServiceImpl.java
@@ -1,11 +1,12 @@
 package ua.kavyar.service.impl;
 
-import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ua.kavyar.model.User;
 import ua.kavyar.service.UserService;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -19,6 +20,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Optional<User> findByEmail(String email) {
-        return Optional.of(new User(login, password, roles));
+        return email.equalsIgnoreCase(login) ?
+                Optional.of(new User(login, password, roles)) : Optional.empty();
     }
 }
